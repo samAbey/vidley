082e3fa0a34cf393c924301c038bd212de8b8e62
Sameera Abeywickrama <sameeraabeywickrama@Sameeras-iMac.local>
2018-09-16 08:55:23 +0530

    FEAT: Add rentals api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const helmet    = require('helmet');
 const genre     = require('./routes/genre');
 const customer  = require('./routes/customer');
 const movie     = require('./routes/movie');
+const rentals   = require('./routes/rentals');
 const mongoose  = require('mongoose');
 
 const app       = express();
@@ -17,7 +18,8 @@ app.use(express.json());
 app.use(helmet());
 app.use('/api/genre', genre);
 app.use('/api/customer', customer);
-app.use('/api/movie', movie)
+app.use('/api/movie', movie);
+app.use('/api/rentals', rentals);
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => { console.log(`Server is listening on ${PORT}`)})
